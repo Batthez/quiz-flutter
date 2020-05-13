8f608327e6d7da60c6d30b52d98e94d5a28e74c9
Snake <gamerbox3@gmail.com>
2020-05-12 22:27:32 -0300

    Tela principal + Play Screen quase completa

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,50 +8,54 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
 
-      body: Container(
-        decoration: BoxDecoration(
-      gradient: LinearGradient(
-      colors: [
-      Color.fromARGB(255, 24, 0, 82),
-        Color.fromARGB(255, 0, 38, 173)
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight
-    ),
-    ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(0.0,0.0,0.0,60.0),
-              child: Text("QUIZ",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.amber,
-                  fontSize: 150.0,
-                  fontFamily: "Times New Roman",
-                ),
+      body: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 24, 0, 82),
+                    Color.fromARGB(255, 0, 38, 173)
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight
               ),
             ),
-            Container(
-              height: 100.0,
-              padding: EdgeInsets.fromLTRB(20.0,0.0,20.0,5.0),
-              child: botao('JOGAR', 60.0, 0, context)
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0.0,0.0,0.0,60.0),
+                  child: Text("QUIZ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 140.0,
+                      fontFamily: "Times New Roman",
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 100.0,
+                    padding: EdgeInsets.fromLTRB(20.0,0.0,20.0,5.0),
+                    child: botao('JOGAR', 60.0, 0, context)
+                ),
+                Container(
+                    height: 80.0,
+                    padding: EdgeInsets.fromLTRB(40.0,0.0,40.0,5.0),
+                    child: botao('CADASTRAR', 40.0, 1, context)
+                ),
+                Container(
+                    height: 70.0,
+                    padding: EdgeInsets.fromLTRB(80.0,0.0,80.0,0.0),
+                    child: botao('LOJA', 35.0, 1, context)
+                ),
+              ],
             ),
-            Container(
-              height: 80.0,
-              padding: EdgeInsets.fromLTRB(40.0,0.0,40.0,5.0),
-              child: botao('CADASTRAR', 40.0, 1, context)
-            ),
-            Container(
-                height: 80.0,
-                padding: EdgeInsets.fromLTRB(80.0,0.0,80.0,0.0),
-                child: botao('LOJA', 35.0, 1, context)
-            ),
-          ],
-        ),
-      ));
+          ),
+      ),
+    );
   }
 RaisedButton botao(String nomebotao, double tamanho, int opcao, BuildContext context){
     return RaisedButton(
--- lib/screens/play_screen.dart
+++ lib/screens/play_screen.dart
@@ -9,24 +9,63 @@ class _PlayScreenState extends State<PlayScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            height: 150,
-            padding: EdgeInsets.all(20),
-            child: RaisedButton(
-                color: Colors.orange,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Colors.black)
-                ),
-                onPressed: (){},
-          )
-          )
-        ],
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Pergunta"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+        Container(
+        padding: EdgeInsets.fromLTRB(10.0,0.0,10.0,0.0),
+        child: botaoescolhas("A. 3500,00cm", 25.0, 0, context)
+        ),
+          Container(
+              padding: EdgeInsets.fromLTRB(10.0,0.0,10.0,0.0),
+              child: botaoescolhas("B. 37,50m", 25.0, 0, context)
+          ),
+          Container(
+              padding: EdgeInsets.fromLTRB(10.0,0.0,10.0,0.0),
+              child: botaoescolhas("C. 0,060m", 25.0, 0, context)
+          ),
+          Container(
+              padding: EdgeInsets.fromLTRB(10.0,0.0,10.0,0.0),
+              child: botaoescolhas("D. 37,50cm", 25.0, 0, context)
+          ),
+        ]
+      ),
+      );
+
+  }
+
+  RaisedButton botaoescolhas(String resposta, double tamanho, int alternativas, BuildContext context) {
+    return RaisedButton(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+            side: BorderSide(color: Colors.black)
+        ),
+        child: Text(
+          resposta,
+          style: TextStyle(
+            fontSize: tamanho,
+            color: Colors.black,
+          ),
+        ),
+        onPressed: () {
+          switch (alternativas) {
+            case 0:
+              break;
+            case 1:
+              break;
+            case 2:
+              break;
+            case 3:
+              break;
+          }
+        }
     );
   }
 }
